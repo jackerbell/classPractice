87a87a22ec6d48d5e8f68e4617fd339f9eccecc2
jackerbell <jackerbell@naver.com>
2022-07-09 22:06:36 +0900

    2022-07-09 update
    footer까지 작업 완료
    내일은 우측 하단 SNS img를 awesome font를 활용해 바꿔볼 예정
    이후 종합적인 분석

--- practice/웹디자인기능사_full/A1/JS/app.js
+++ practice/웹디자인기능사_full/A1/JS/app.js
@@ -1,34 +1,48 @@
 $(document).ready(function(){
-  //DOM이 완성되면 실행된다.
-  menu()
-  $('#slide_contents').bxSlider({
-    auto: true,
-    speed:400,
-    pause:3000,
-    mode:'horizontal',
-    autoHover:true,
-    pager:true
-    /* 여기에다가 slideWidth:1200, slideHeight:300 을 작성해보았으나 변화가 없었음
-    그래서 직접 jquery.bxslider.css 파일에서 직접 수정하기로 함 */
-  })
+    //DOM이 완성되면 실행된다.
+    menu()
+    $('#slide_contents').bxSlider({
+        auto: true,
+        speed:400,
+        pause:3000,
+        mode:'horizontal',
+        autoHover:true,
+        pager:true
+        /* 여기에다가 slideWidth:1200, slideHeight:300 을 작성해보았으나 변화가 없었음
+        그래서 직접 jquery.bxslider.css 파일에서 직접 수정하기로 함 */
+    })
+    tab()
 })
 
 function menu(){
-  $("#gnb>li").hover(
-      //mouseover
-      function(){
-          console.log('mouseover')
-          $(".lnb").stop().slideDown()
-          //chainging기법, stop을 사용해서 이전 애니메이션 중지
-          //->드랍다운 메뉴가 자연스럽게 된다.
-          //stop을 안쓰면 애니메이션이 큐에 쌓여서...중첩...
-      },
-      //mouseout
-      function(){
-          console.log('mouseout')
-          $(".lnb").stop().slideUp()
-      }
-  )
+    $("#gnb>li").hover(
+        //mouseover
+        function(){
+            console.log('mouseover')
+            $(".lnb").stop().slideDown()
+            //chainging기법, stop을 사용해서 이전 애니메이션 중지
+            //->드랍다운 메뉴가 자연스럽게 된다.
+            //stop을 안쓰면 애니메이션이 큐에 쌓여서...중첩...
+        },
+        //mouseout
+        function(){
+            console.log('mouseout')
+            $(".lnb").stop().slideUp()
+        }
+    )
+}
+
+function tab(){
+    $('#tab-btn>a').eq(0).click(
+        function(){
+            $('#notice').show()
+            $('#gallery').hide()
+        })
+    $('#tab-btn>a').eq(1).click(
+        function(){
+          $('#notice').hide()
+          $('#gallery').show()
+        })
 }
 /* 
 no Jquery plugIn version (강의 자료)
