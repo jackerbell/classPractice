714e5ae38409e440a1d9c6a168724dca0ca0c4de
jackerbell <jackerbell@naver.com>
2022-07-27 23:41:54 +0900

    2022-07-27 update
    두번째 웹기능사 문제 풀이 시작
    프레임 작업 및 로고 메뉴 작업 완료

--- practice/웹디자인기능사_full/A1/JS/app.js
+++ practice/웹디자인기능사_full/A1/JS/app.js
@@ -1,68 +1,68 @@
-$(document).ready(function(){
-    //DOM이 완성되면 실행된다.
-    menu()
-    $('#slide_contents').bxSlider({
-        auto: true,
-        speed:400,
-        pause:3000,
-        mode:'horizontal',
-        autoHover:true,
-        pager:true
-        /* 여기에다가 slideWidth:1200, slideHeight:300 을 작성해보았으나 변화가 없었음
-        그래서 직접 jquery.bxslider.css 파일에서 직접 수정하기로 함 */
-    })
-    popup()
-    tab()
+$(document).ready(function () {
+  //DOM이 완성되면 실행된다.
+  menu()
+  $('#slide_contents').bxSlider({
+    auto: true,
+    speed: 400,
+    pause: 3000,
+    mode: 'horizontal',
+    autoHover: true,
+    pager: true
+    /* 여기에다가 slideWidth:1200, slideHeight:300 을 작성해보았으나 변화가 없었음
+    그래서 직접 jquery.bxslider.css 파일에서 직접 수정하기로 함 */
+  })
+  popup()
+  tab()
 })
 
-function menu(){
-    $("#gnb>li").hover(
-        //mouseover
-        function(){
-            console.log('mouseover')
-            $(".lnb").stop().slideDown()
-            //chainging기법, stop을 사용해서 이전 애니메이션 중지
-            //->드랍다운 메뉴가 자연스럽게 된다.
-            //stop을 안쓰면 애니메이션이 큐에 쌓여서...중첩...
-        },
-        //mouseout
-        function(){
-            console.log('mouseout')
-            $(".lnb").stop().slideUp()
-        }
-    )
+function menu() {
+  $("#gnb>li").hover(
+    //mouseover
+    function () {
+      console.log('mouseover')
+      $(".lnb").stop().slideDown()
+      //chainging기법, stop을 사용해서 이전 애니메이션 중지
+      //->드랍다운 메뉴가 자연스럽게 된다.
+      //stop을 안쓰면 애니메이션이 큐에 쌓여서...중첩...
+    },
+    //mouseout
+    function () {
+      console.log('mouseout')
+      $(".lnb").stop().slideUp()
+    }
+  )
 }
 
-function tab(){
-    $('#tab-btn>a').eq(0).click(
-        function(){
-            $('#notice').show()
-            $('#gallery').hide()
-        })
-    $('#tab-btn>a').eq(1).click(
-        function(){
-          $('#notice').hide()
-          $('#gallery').show()
-        })
+function tab() {
+  $('#tab-btn>a').eq(0).click(
+    function () {
+      $('#notice').show()
+      $('#gallery').hide()
+    })
+  $('#tab-btn>a').eq(1).click(
+    function () {
+      $('#notice').hide()
+      $('#gallery').show()
+    })
 }
 
 function popup() {
-    if($.cookie('popup') == 'none') {
-        $('#popup').hide();
-    }
+  if ($.cookie('popup') == 'none') {
+    $('#popup').hide();
+  }
 
-    var $expchk = $('#expchk')
-    $('#btnX').on('click', closePop);
+  var $expchk = $('#expchk')
+  $('#btnX').on('click', closePop);
 
-    function closePop(){
-        if($expchk.is(':checked')) {
-            $.cookie('popup','none',{
-                expires: 7,
-                path: '/'
-            });
-        }
-        $('#popup').fadeOut('fast');
+  function closePop() {/*쿠키 생성 */
+    if ($expchk.is(':checked')) {
+      $.cookie('popup', 'none', {
+        expires: 7,
+        path: '/'
+      });
     }
+    $('#popup').fadeOut('fast');
+  }
 }
 /* 
 no Jquery plugIn version (강의 자료)
